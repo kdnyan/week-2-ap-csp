7d64fc5d4ffd060fbe73afcde3ac9b098124182b
Keion Yan <jay.walk3r911@gmail.com>
2025-10-31 17:46:36 +0000

    advanced strings

--- week_11_day2.py
+++ week_11_day2.py
@@ -9,25 +9,25 @@ print("You’ll review variables, strings, numbers, and print formatting.\n")
 
 # Step 1: Create Variables
 # TODO: Replace the values below with your own info
-first_name = "Marvin"
-age = 30
-favorite_color = "Blue"
-favorite_number = 7
+first_name = "Tommy"
+age = 18
+favorite_color = "white"
+favorite_number = 8
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
-
+print(first_name.upper())
 
 # 2. Print how many letters are in your name
-
+print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print((first_name), (favorite_color))
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
-
-
+x = 10
+print(str(x) + str(favorite_number))
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
 
